0598193dcd1dadde17981dd4f776652904ade534
MaiconMJS <newoverride@gmail.com>
2025-04-21 14:48:44 -0300

    Tamanho dos botões alterados

--- app/src/main/java/com/newoverride/incrementador/dimens/Dimens.kt
+++ app/src/main/java/com/newoverride/incrementador/dimens/Dimens.kt
@@ -7,7 +7,7 @@ object Dimens {
     val displayTextSize = 50.sp
     val spacerDisplayButton = 20.dp
     val buttonTextSize = 12.sp
-    val heightButton = 40.dp
-    val widthButton = 150.dp
+    val heightButton = 50.dp
+    val widthButton = 160.dp
     val spacerButtonStopZero = 20.dp
 }
