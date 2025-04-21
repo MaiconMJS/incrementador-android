e8040053ae8b38452f4aa95c41a44be89a7f792b
MaiconMJS <newoverride@gmail.com>
2025-04-21 19:04:54 -0300

    Tema claro & escuro dynamic

--- app/src/main/java/com/newoverride/incrementador/ui/view/HomeView.kt
+++ app/src/main/java/com/newoverride/incrementador/ui/view/HomeView.kt
@@ -8,6 +8,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,7 +20,7 @@ import com.newoverride.incrementador.enum.ButtonType
 import com.newoverride.incrementador.hooks.incrementHook
 import com.newoverride.incrementador.ui.components.ButtonStyle
 import com.newoverride.incrementador.ui.components.Display
-import com.newoverride.incrementador.ui.theme.Black
+import com.newoverride.incrementador.ui.theme.BlackLow
 import com.newoverride.incrementador.ui.theme.Blue
 import com.newoverride.incrementador.ui.theme.Green
 import com.newoverride.incrementador.ui.theme.IncrementadorTheme
@@ -30,44 +32,46 @@ fun HomeView() {
     val incrementHook = incrementHook()
 
     IncrementadorTheme {
-        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-            Column {
-                Display(incrementHook.number["n1"] ?: 0, incrementHook.number["n2"] ?: 0)
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    ButtonStyle(
-                        textButton = stringResource(R.string.ManualButton),
-                        onClick = incrementHook.manualIncrement,
-                        isPressed = incrementHook.buttonPressedState[ButtonType.MANUAL] == true,
-                        colors = ButtonDefaults.buttonColors(containerColor = Blue)
-                    )
-                    ButtonStyle(
-                        textButton = stringResource(R.string.AutoButton),
-                        onClick = incrementHook.autoIncrement,
-                        isPressed = incrementHook.buttonPressedState[ButtonType.AUTO] == true,
-                        colors = ButtonDefaults.buttonColors(containerColor = Green)
-                    )
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = Dimens.spacerButtonStopZero),
-                    horizontalArrangement = Arrangement.SpaceAround
-                ) {
-                    ButtonStyle(
-                        textButton = stringResource(R.string.StopButton),
-                        onClick = incrementHook.stopIncrement,
-                        isPressed = incrementHook.buttonPressedState[ButtonType.STOP] == true,
-                        colors = ButtonDefaults.buttonColors(containerColor = Red)
-                    )
-                    ButtonStyle(
-                        textButton = stringResource(R.string.ZeroButton),
-                        onClick = incrementHook.zeroDisplay,
-                        isPressed = incrementHook.buttonPressedState[ButtonType.ZERO] == true,
-                        colors = ButtonDefaults.buttonColors(containerColor = Black)
-                    )
+        Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Column {
+                    Display(incrementHook.number["n1"] ?: 0, incrementHook.number["n2"] ?: 0)
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        ButtonStyle(
+                            textButton = stringResource(R.string.ManualButton),
+                            onClick = incrementHook.manualIncrement,
+                            isPressed = incrementHook.buttonPressedState[ButtonType.MANUAL] == true,
+                            colors = ButtonDefaults.buttonColors(containerColor = Blue)
+                        )
+                        ButtonStyle(
+                            textButton = stringResource(R.string.AutoButton),
+                            onClick = incrementHook.autoIncrement,
+                            isPressed = incrementHook.buttonPressedState[ButtonType.AUTO] == true,
+                            colors = ButtonDefaults.buttonColors(containerColor = Green)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = Dimens.spacerButtonStopZero),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        ButtonStyle(
+                            textButton = stringResource(R.string.StopButton),
+                            onClick = incrementHook.stopIncrement,
+                            isPressed = incrementHook.buttonPressedState[ButtonType.STOP] == true,
+                            colors = ButtonDefaults.buttonColors(containerColor = Red)
+                        )
+                        ButtonStyle(
+                            textButton = stringResource(R.string.ZeroButton),
+                            onClick = incrementHook.zeroDisplay,
+                            isPressed = incrementHook.buttonPressedState[ButtonType.ZERO] == true,
+                            colors = ButtonDefaults.buttonColors(containerColor = BlackLow)
+                        )
+                    }
                 }
             }
         }
